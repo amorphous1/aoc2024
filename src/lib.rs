10eae6ac8d89858a9eb00bf9a659ad0c4c81f8c9
amorphous1 <harrystew@gmail.com>
2024-12-02 21:01:49 +0000

    add day 2 part 1 solutions

--- src/lib.rs
+++ src/lib.rs
@@ -37,6 +37,25 @@ pub fn day01b(input: &str) -> u32 {
     return result;
 }
 
+fn is_safe(report: Vec<i32>) -> bool {
+    let mut last_sig = (report[1] - report[0]).signum();
+    for i  in 1..report.len() {
+        let delta = report[i] - report[i-1];
+        if delta.signum() != last_sig || delta.abs() < 1 || delta.abs() > 3 {
+            return false;
+        }
+        last_sig = delta.signum();
+    }
+    return true;
+}
+
+pub fn day02a(input: &str) -> usize {
+    return input.split('\n').filter(|line| {
+        let report = line.split_ascii_whitespace().map(|num| num.parse().unwrap()).collect();
+        return is_safe(report);
+    }).count();
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -52,5 +71,17 @@ mod tests {
 
         assert_eq!(day01a(sample_input), 11);
         assert_eq!(day01b(sample_input), 31);
+    }
+
+    #[test]
+    fn day02_samples() {
+        let sample_input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+
+        assert_eq!(day02a(sample_input), 2);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::fs;
 
 fn main() -> Result<(), std::io::Error> {
-    let day01_input = &fs::read_to_string("inputs/day01.input")?;
-    println!("{}", aoc2024::day01a(day01_input));
-    println!("{}", aoc2024::day01b(day01_input));
+    // let day01_input = &fs::read_to_string("inputs/day01.input")?;
+    // println!("{}", aoc2024::day01a(day01_input));
+    // println!("{}", aoc2024::day01b(day01_input));
+
+    let day02_input = &fs::read_to_string("inputs/day02.input")?;
+    println!("{}", aoc2024::day02a(day02_input));
 
     Ok(())
 }
