8cdff436f8d8566a47a078d709be7320d14335c7
amorphous1 <harrystew@gmail.com>
2024-12-14 18:18:55 +0000

    add day 14 part 2 solution

--- src/day14.rs
+++ src/day14.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use regex::Regex;
 
 pub fn safety_factor(input: &str, steps: u16, width: u16, height: u16) -> u64 {
@@ -25,6 +26,42 @@ pub fn safety_factor(input: &str, steps: u16, width: u16, height: u16) -> u64 {
         quadrants[quadrant] += 1;
     }
     return quadrants[1] * quadrants[2] * quadrants[3] * quadrants[4];
+}
+
+pub fn print_robots(input: &str, steps: u16, width: u16, height: u16) {
+    let line_regex = Regex::new(r"p=([-0-9]+),([-0-9]+) v=([-0-9]+),([-0-9]+)").unwrap();
+    let mut robots: HashSet<(i64, i64)> = HashSet::new();
+    for (_, [px, py, vx, vy]) in line_regex.captures_iter(input).map(|c| c.extract()) {
+        let (px, py, vx, vy) = (px.parse::<i64>().unwrap(), py.parse::<i64>().unwrap(), vx.parse::<i64>().unwrap(), vy.parse::<i64>().unwrap());
+        let mut px = (px + vx * steps as i64) % width as i64;
+        while px < 0 {
+            px += width as i64;
+        }
+        let mut py = (py + vy * steps as i64) % height as i64;
+        while py < 0 {
+            py += height as i64;
+        }
+        robots.insert((px, py));
+    }
+    if is_print_candidate(&robots) {
+        println!("{} ==================================================================================", steps);
+        for y in 0..height as i64 {
+            for x in 0..width as i64 {
+                print!("{}", if robots.contains(&(x, y)) { '#' } else { ' ' });
+            }
+            println!();
+        }
+    }
+}
+
+fn is_print_candidate(robots: &HashSet<(i64, i64)>) -> bool {
+    for (x,y) in robots {
+        if robots.contains(&(*x + 1, *y)) && robots.contains(&(*x + 2, *y)) && robots.contains(&(*x + 3, *y)) && robots.contains(&(*x + 4, *y)) &&
+            robots.contains(&(*x, *y + 1)) && robots.contains(&(*x, *y + 2)) && robots.contains(&(*x, *y + 3)) && robots.contains(&(*x, *y + 4)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -17,59 +17,62 @@ mod day13;
 mod day14;
 
 fn main() -> Result<(), std::io::Error> {
-    let day01_input = &fs::read_to_string("inputs/day01.txt")?;
-    println!("{}", day01::part1(day01_input));
-    println!("{}", day01::part2(day01_input));
-
-    let day02_input = &fs::read_to_string("inputs/day02.txt")?;
-    println!("{}", day02::is_safe(day02_input, false));
-    println!("{}", day02::is_safe(day02_input, true));
-
-    let day03_input = &fs::read_to_string("inputs/day03.txt")?;
-    println!("{}", day03::part1(day03_input));
-    println!("{}", day03::part2(day03_input));
-
-    let day04_input = &fs::read_to_string("inputs/day04.txt")?;
-    println!("{}", day04::part1(day04_input));
-    println!("{}", day04::part2(day04_input));
-
-    let day05_input = &fs::read_to_string("inputs/day05.txt")?;
-    println!("{}", day05::day05(day05_input, true));
-    println!("{}", day05::day05(day05_input, false));
-
-    let day06_input = &fs::read_to_string("inputs/day06.txt")?;
-    print_and_time(|| day06::visited_positions(day06_input).len());
-    print_and_time(|| day06::loop_obstructions(day06_input));
-
-    let day07_input = &fs::read_to_string("inputs/day07.txt")?;
-    print_and_time(|| day07::sum_solvable_equations(day07_input, false));
-    print_and_time(|| day07::sum_solvable_equations(day07_input, true));
-
-    let day08_input = &fs::read_to_string("inputs/day08.txt")?;
-    print_and_time(|| day08::unique_antinodes(day08_input));
-    print_and_time(|| day08::unique_antinodes2(day08_input));
-
-    let day09_input = &fs::read_to_string("inputs/day09.txt")?;
-    print_and_time(|| day09::checksum(day09_input));
-    print_and_time(|| day09::checksum2(day09_input));
-
-    let day10_input = &fs::read_to_string("inputs/day10.txt")?;
-    println!("{:?}", day10::trailhead_scores(day10_input));
-
-    let day11_input = &fs::read_to_string("inputs/day11.txt")?;
-    print_and_time(|| day11::day11(day11_input, 25));
-    print_and_time(|| day11::day11(day11_input, 75));
-
-    let day12_input = &fs::read_to_string("inputs/day12.txt")?;
-    print_and_time(|| day12::fence_price(day12_input, false));
-    print_and_time(|| day12::fence_price(day12_input, true));
-
-    let day13_input = &fs::read_to_string("inputs/day13.txt")?;
-    print_and_time(|| day13::min_tokens(day13_input, 0));
-    print_and_time(|| day13::min_tokens(day13_input, 10000000000000));
-
+    // let day01_input = &fs::read_to_string("inputs/day01.txt")?;
+    // println!("{}", day01::part1(day01_input));
+    // println!("{}", day01::part2(day01_input));
+    //
+    // let day02_input = &fs::read_to_string("inputs/day02.txt")?;
+    // println!("{}", day02::is_safe(day02_input, false));
+    // println!("{}", day02::is_safe(day02_input, true));
+    //
+    // let day03_input = &fs::read_to_string("inputs/day03.txt")?;
+    // println!("{}", day03::part1(day03_input));
+    // println!("{}", day03::part2(day03_input));
+    //
+    // let day04_input = &fs::read_to_string("inputs/day04.txt")?;
+    // println!("{}", day04::part1(day04_input));
+    // println!("{}", day04::part2(day04_input));
+    //
+    // let day05_input = &fs::read_to_string("inputs/day05.txt")?;
+    // println!("{}", day05::day05(day05_input, true));
+    // println!("{}", day05::day05(day05_input, false));
+    //
+    // let day06_input = &fs::read_to_string("inputs/day06.txt")?;
+    // print_and_time(|| day06::visited_positions(day06_input).len());
+    // print_and_time(|| day06::loop_obstructions(day06_input));
+    //
+    // let day07_input = &fs::read_to_string("inputs/day07.txt")?;
+    // print_and_time(|| day07::sum_solvable_equations(day07_input, false));
+    // print_and_time(|| day07::sum_solvable_equations(day07_input, true));
+    //
+    // let day08_input = &fs::read_to_string("inputs/day08.txt")?;
+    // print_and_time(|| day08::unique_antinodes(day08_input));
+    // print_and_time(|| day08::unique_antinodes2(day08_input));
+    //
+    // let day09_input = &fs::read_to_string("inputs/day09.txt")?;
+    // print_and_time(|| day09::checksum(day09_input));
+    // print_and_time(|| day09::checksum2(day09_input));
+    //
+    // let day10_input = &fs::read_to_string("inputs/day10.txt")?;
+    // println!("{:?}", day10::trailhead_scores(day10_input));
+    //
+    // let day11_input = &fs::read_to_string("inputs/day11.txt")?;
+    // print_and_time(|| day11::day11(day11_input, 25));
+    // print_and_time(|| day11::day11(day11_input, 75));
+    //
+    // let day12_input = &fs::read_to_string("inputs/day12.txt")?;
+    // print_and_time(|| day12::fence_price(day12_input, false));
+    // print_and_time(|| day12::fence_price(day12_input, true));
+    //
+    // let day13_input = &fs::read_to_string("inputs/day13.txt")?;
+    // print_and_time(|| day13::min_tokens(day13_input, 0));
+    // print_and_time(|| day13::min_tokens(day13_input, 10000000000000));
+    //
     let day14_input = &fs::read_to_string("inputs/day14.txt")?;
     print_and_time(|| day14::safety_factor(day14_input, 100, 101, 103));
+    for steps in 0..10000 {
+            day14::print_robots(day14_input, steps, 101, 103);
+    }
 
     Ok(())
 }
